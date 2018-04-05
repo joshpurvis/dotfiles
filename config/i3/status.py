dc0a255ee40e3e63273f9c244ea99620e2e4019b
Josh Purvis <joshua.purvis@gmail.com>
2018-04-05 17:47:05 -0400

    fix issues with xprofile/xmr stats/numlock

--- config/i3/status.py
+++ config/i3/status.py
@@ -25,8 +25,7 @@ status.register("cpu_usage")
 #status.register("shell", command='echo "CPU $(sensors | grep -A 0 \'temp1\' | cut -c16-21)"',)
 
 status.register("mem", divisor=1024**3, color=None, warn_color=None, format="{avail_mem} GB")
-
-status.register(Monero, address=os.environ.get('MONERO_ADDRESS'))
+status.register(Monero, address=os.environ.get('MONERO'))
 
 # tun0 up/down
 #status.register("network", format_up = "{interface}{kbs} KB/s", start_color="#689d6a", format_down="{interface}", interface="tun0", color_up="#689d6a", color_down="#AA3939", dynamic_color=False, on_rightclick=False, on_upscroll=False, on_downscroll=False, on_leftclick=False, graph_width=1, unknown_up=True)
